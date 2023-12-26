c7f977a6858554f211b99d701097a9b29faef963
Pradipkumar Desai <pradipkumardesai@live.in>
2023-12-26 15:44:57 +0530

    Add project files.

---
+++ PD.DesignPattern.Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PD.DesignPattern.Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
